7af793c190cf4150d71dc14376e35c9251f568fa
GustavoBiaso <gustavobiaso@gmail.com>
2024-06-13 01:58:13 -0300

    First Commit

---
+++ src/test/java/com/br/traineemakers/apiTask/ApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.traineemakers.apiTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
